312d5385f9e931ae5b4d04161a82245a0a2d2a45
rafaelrc7 <contact@rafaelrc.com>
2022-11-01 13:08:06 -0300

    Make environment

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.default = pkgs.mkShell { nativeBuildInputs = with pkgs; [ llvm clang clang-tools ]; };
+    }
+  );
+}
+
